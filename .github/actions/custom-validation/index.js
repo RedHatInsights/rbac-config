068c5c119af0d8bfe5d1f9a5e129068071a5b0e3
Keith Walsh <kwalsh@redhat.com>
2021-10-13 12:50:15 -0400

    Add custom validation action
    This adds a relative Javascript action [1,2] into the repo, as part of the `pull_request`
    workflow we currently have defined [3].
    
    You can test the action locally by installing `act` [4] and running `act pull_request`.
    
    We will now also be limiting the fields in permission details to those defined in
    the schema, via:
    ```
    "additionalProperties": false
     ```
    
    [1] https://docs.github.com/en/actions/learn-github-actions/finding-and-customizing-actions#referencing-an-action-in-the-same-repository-where-a-workflow-file-uses-the-action
    [2] https://docs.github.com/en/actions/creating-actions/creating-a-javascript-action
    [3] https://github.com/RedHatInsights/rbac-config/blob/master/.github/workflows/pr.yml
    [4] https://github.com/nektos/act

---
+++ .github/actions/custom-validation/index.js
@@ -0,0 +1,43 @@
+const core = require('@actions/core')
+const github = require('@actions/github')
+const fs = require("fs")
+const glob = require("glob")
+
+try {
+  const permissionsPath = core.getInput('permissions_path_pattern')
+
+  glob(permissionsPath, {}, function(er, files) {
+    files.forEach(file => {
+      let permissions = JSON.parse(fs.readFileSync(file, 'utf8'))
+      let resourceDefinitions = Object.keys(permissions)
+
+      resourceDefinitions.forEach(resourceDefinition => {
+        let permissionDetails = permissions[resourceDefinition]
+        let verbsForResource = permissionDetails.map(detail =>
+          Object.keys(detail).filter(detailKey =>
+            detailKey == 'verb'
+          ).map(detailKey =>
+            detail[detailKey]
+          ).reduce((prev, curr) =>
+            prev.concat(curr)
+          )
+        )
+
+        permissionDetails.forEach(detail => {
+          let requires = detail.requires
+          if (requires) {
+            requires.forEach(requiredVerb => {
+              let valid = verbsForResource.includes(requiredVerb)
+              if (!valid) {
+                let err = `The required verb "${requiredVerb}" is not one of ${JSON.stringify(verbsForResource)}`
+                core.setFailed(err)
+              }
+            })
+          }
+        })
+      })
+    })
+  })
+} catch (error) {
+  core.setFailed(error.message)
+}
